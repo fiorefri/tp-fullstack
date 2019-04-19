3bf75cce0ad1ba0329c78e6c2211c8294041a81f
fiorefri <fiorefri@gmail.com>
2019-04-19 02:25:58 -0300

    creacion login.php

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
+    <title>LOGIN</title>
+  </head>
+  <body>
+    <main>
+      <h1>Iniciar Sesion</h1>
+      <form class="" action="index.php" method="post">
+        <label for="email">E-mail</label>
+        <input type="text" name="email" value="" placeholder="E-mail">
+        <label for="contraseña">Contraseña</label>
+        <input type="pass" name="contraseña" value="" placeholder="Contraseña">
+        <section class="ingreso-forgot">
+          <button type="submit">Ingresar</button>
+          <a href="#" class="forgot">¿Olvidaste tu contraseña?</a>
+        </section>
+      </form>
+      <hr>
+      <section class="registro">
+        <h2>¿No tenes cuenta?</h2>
+        <a href="#registro.php" class="registrate">Registrate</a>
+      </section>
+    </main>
+  </body>
+</html>
