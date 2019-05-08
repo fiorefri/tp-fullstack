bfd7c5d7366663274ed81d318550df5362a07684
fiorefri <fiorefri@gmail.com>
2019-05-08 12:45:05 -0300

    se junta nombre y apellido y se agrega una segunda contraseña

--- login.php
+++ login.php
@@ -15,9 +15,9 @@
       <h1>Iniciar Sesión</h1>
       <form class="" action="index.php" method="post">
         <label for="email">E-mail</label>
-        <input type="text" name="email" value="" placeholder="E-mail">
+        <input type="pass" name="email" value="" placeholder="E-mail">
         <label for="contraseña">Contraseña</label>
-        <input type="pass" name="contraseña" value="" placeholder="Contraseña">
+        <input type="pass" name="pass" value="" placeholder="Contraseña">
         <section class="ingreso-forgot">
           <button type="submit">Ingresar</button>
           <a href="#" class="forgot">¿Olvidaste tu contraseña?</a>
