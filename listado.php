54fb3d8f7d7f1138f04e04d8841c6cf04bc0c501
flor1985 <florgiles@hotmail.com>
2019-05-15 14:33:24 -0300

    Se modifica pagina de Ver/Editar usuario

--- listado.php
+++ listado.php
@@ -7,3 +7,20 @@ $usuarios = listaUsuarios();
 var_dump($_SESSION);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Franklin|Playfair+Display" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Ver/ Editar usuario</title>
+  </head>
+  <body>
+    <?php include_once "header.php" ?>
+  </body>
+</html>
+<?php var_dump($usuarios);?>
