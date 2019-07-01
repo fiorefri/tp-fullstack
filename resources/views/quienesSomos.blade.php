c9dc7a69a209a16a86fc493871b9f77370f3d72d
fiorefri <fiorefri@gmail.com>
2019-07-01 12:21:56 -0300

    se agrega Laravel al proyecto

---
+++ resources/views/quienesSomos.blade.php
@@ -0,0 +1,15 @@
+@extends('plantilla')
+
+@section('titulo', "Quienes Somos")
+
+@section('principal')
+  <div class="about-us">
+    <p><strong>Nature</strong> es una marca joven, enfocada en crear verdadera cosmética natural, cuidando hasta los mas mínimos detalles en cuanto a ingredientes, producción y envasado, utilizando materias primas naturales de la mas alta calidad. <br>A pesar del corto camino transitado, nuestro equipo tiene basta experiencia en el rubro de materias primas cosméticas.
+    </p>
+    <h5><strong>Por que decimos que creamos verdadera cosmética natural?</strong></h5>
+    <p>En <strong>Nature</strong> elegimos rigurosamente cada materia prima utilizada en todas nuestras líneas, importamos nosotros mismos casi la totalidad de ingredientes, analizando previamente cada uno de ellos para certificar la calidad de los mismos.<br>Nuestros productos se fabrican a base de aceites vegetales puros, tales como aceite de almendras, aceite de coco, aceite de argán, aceite de jojoba, manteca de karité, entre otros. Evitamos en todo proceso y en nuestras formulas materia primas derivadas del petróleo, tales como parafina, vaselina, siliconas, entre otras
+    </p>
+    <h5><strong>¿Que nos diferencia?</strong></h5>
+    <p>Nuestros productos están certificados por ANMAT, certificados SIN TACC, no testeamos en animales y no utilizamos parabenos.<br>Nuestros productos orgánicos estan certificado por ECO-CERT (certificadora de origen Francés).</p>
+  </div>
+@endsection
