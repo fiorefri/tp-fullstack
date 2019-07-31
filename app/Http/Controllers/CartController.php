6f733493ceca4879ea20134c96bb7afd2b2bbb72
JudithGrau <judith.betina.grau@gmail.com>
2019-07-31 16:23:05 -0300

    Cartcontroller y ProductController. Routes

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -14,7 +14,31 @@ class CartController extends Controller
      */
     public function index()
     {
-        //
+        $carts = Cart::where('status',0)
+        ->where('user_id', Auth::user()->id)
+        ->get(); //Identificamos todos los items de los carritos abiertos del usuario logueado.
+
+        $carts = Cart::all()->where('status',0)
+        ->where('user_id', Auth::user()->id); //Identificamos todos los items de los carritos abiertos del usuario logueado.
+
+        // Cálculo del total usando el método `getTotal()` del modelo
+        // $cart = new Cart();
+        // $total2 = $cart->getTotal($carts);
+
+        // Calculo del total usando `reduce()`. Lo vemos el miercoles :D
+        // $total = $carts->reduce(function($suma, $item){
+        //   $suma += $item->price * $item->quantity;
+        //   return $suma;
+        // });
+
+        //Calculamos el total de cada carrito.
+        $total = 0;
+        foreach ($carts as $item) {
+          $total = $total +($item->quantity * $item->price);
+          }
+
+        return view('cart', compact('carts', 'total'));
+          //
     }
 
     /**
@@ -35,7 +59,37 @@ class CartController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Validamos que el número ingresado sea positivo.
+        $rule = [
+          'quantity'=>"integer|min:1"
+        ];
+        $message = [
+          'min'=>'La cantidad debe ser mayor a :min.',
+          'integer'=>'El valor debe ser numérico.'
+        ];
+
+        $this->validate($request, $rule, $message);
+
+        $product = Product::find($request->id);
+
+        //El producto ya está cargado en el cart ->necesitamos el id de producto en la tabla: crear m,igration.
+        $existe = Cart::where('product_id', $request->id)->where('user_id',Auth::user()->id)->where('status','0')->first();
+
+        if($existe){
+          $existe->quantity += $request->quantity;
+          $existe->save();
+          return redirect('/products');
+        }
+
+        $cart = new Cart; //Recordar que cada Cart es un item dentro del carrito.
+        $cart->product_id = $product->id;
+        $cart->name = $product->name;
+        $cart->price = $product->price;
+        $cart->quantity = $request->quantity;
+        $cart->user_id = Auth::user()->id;
+
+        $cart->save();
+        return redirect('/products');
     }
 
     /**
@@ -78,8 +132,44 @@ class CartController extends Controller
      * @param  \App\cart  $cart
      * @return \Illuminate\Http\Response
      */
-    public function destroy(cart $cart)
-    {
-        //
-    }
+     public function destroy($id)
+     {
+         $item = Cart::where('id',$id)
+         ->where('user_id',Auth::user()->id)
+         ->where('status',0)->get(); //La consulta nos devuelve un array de datos con 1 solo item.
+
+         $item[0]->delete(); //El item está en la posición 0 del array. Lo eliminamos.
+
+         //Otra forma de traer el item a borrar es usando first():
+         // $item = Cart::where('id',$id)
+         // ->where('user_id',Auth::user()->id)
+         // ->where('status',0)->first();
+         // $item->delete();
+
+         return redirect('/cart');
+     }
+     public function cartClose(){
+       $items = Cart::where('user_id',Auth::user()->id)
+       ->where('status',0)->get();
+
+       $lastCart = Cart::max('cart_num'); //Busca el último cerrado.
+
+       foreach ($items as $item) {
+         $item->cart_num = $lastCart + 1;
+         $item->status = 1;
+         $item->save();
+       }
+
+       return redirect ('/products');
+     }
+
+     public function history(){
+       $carts = Cart::where('user_id',Auth::user()->id)
+       ->where('status',1)->get()->groupBy('cart_num');
+
+       return view('history', compact('carts'));
+
+
+     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,20 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/products', 'ProductController@index'); //Listamos productos
+
+Route::get('/product/{id}', 'ProductController@show'); //Mostramos 1 producto
+
+Route::post('/addtocart', 'CartController@store')->middleware('auth'); //Guardamos productos en tabla cart.
+Route::get('/delete/{id}', 'CartController@destroy')->middleware('auth'); //Borramos productos del carrito.
+
+Route::get('/cart', 'CartController@index')->middleware('auth'); //Mostramos el carrito abierto.
+Route::post('/cartclose', 'CartController@cartClose')->middleware('auth');//carrito cerrado
+
+Route::get('/history', 'CartController@history')->middleware('auth');//historial de compras
+Route::get('/category/{id?}', 'ProductController@index');//categorias de productos
+
+Route::get('/cart', 'CartController@show');//mostrar carrito
+
+Route::get('/home', 'QuienesSomosController@index')->name('home')
