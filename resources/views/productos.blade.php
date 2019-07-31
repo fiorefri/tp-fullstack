114fb39efda67f5b18ebcfd87d4252a4c6c836d3
JudithGrau <judith.betina.grau@gmail.com>
2019-07-31 18:13:42 -0300

    se modifica productos.blade

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -45,6 +45,58 @@
     </div>
   </div>
 
+  <div class="crema_rostro row">
+    <div class="articulo col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/aguaMicelarDesmaquillante.png" alt="">
+      <br>
+      <a href="#">Agua micelar desmaquillante</a>
+    </div>
+    <div class="articulo col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/brumaDeArgan.png" alt="">
+      <br>
+      <a href="#">Bruma de Argan</a>
+    </div>
+    <div class="articulo col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/contornoDeOjosRevitalizante.png" alt="">
+      <a href="#">Contorno de ojos revitalizante</a>
+    </div>
+    <div class="articulo  col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/cremaGelDeDia.png" alt="">
+      <br>
+      <a href="#">Crema gel de dia</a>
+    </div>
+    <div class="articulo col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/cremaGelDeNoche.png" alt="">
+      <br>
+      <a href="#">Crema gel de noche</a>
+    </div>
+    <div class="articulo col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/macarilla-carbon.jpg" alt="">
+      <br>
+      <a href="#">Mascarilla carbón</a>
+    </div>
+    <div class="articulo col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/serum-antiarrugas.JPG" alt="">
+      <br>
+      <a href="#">Serum anti-arrugas</a>
+    </div>
+    <div class="articulo col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/serumAcidoHialuronico.png" alt="">
+      <br>
+      <a href="#">Serum acido hialurónico</a>
+    </div>
+    <div class="articulo col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/serumDeCalendula.png" alt="">
+      <br>
+      <a href="#">Serum de caléndula</a>
+    </div>
+    <div class="articulo col-xs-12 col-md-6 col-lg-4">
+      <img class="img-fluid"src="img/rostro/tonico-facial.JPG" alt="">
+      <br>
+      <a href="#">Tónico facial</a>
+    </div>
+  </div>
+
 
 
 @endsection
