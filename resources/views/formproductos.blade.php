7a9fa7e9fbc65271f87d1ba521ac03c06fe0d132
JudithGrau <judith.betina.grau@gmail.com>
2019-08-02 15:53:52 -0300

    Se creaformulario productos.blade

--- resources/views/formproductos.blade.php
+++ resources/views/formproductos.blade.php
@@ -5,7 +5,47 @@
 @section('class-body', 'container')
 
 @section('principal')
+@csrf
+{{-- {{csrf_field()}} --}}
 <form class="" action="/productosAgregar" method="post">
+
+  <p>
+    <input type="text" name="nombre" value="" placeholder="nombre producto">
+  </p>
+  <p>
+    <input type="text" name="descripcion" value="" placeholder="descripción producto">
+  </p>
+  <p>
+    <input type="number_format" name="precio" value="" placeholder="precio producto">
+  </p>
+  <p>
+    <input type="number" name="stock" value="" placeholder="stock">
+  </p>
+  <p>
+    <input type="text" name="id_category" value="" placeholder="categoria">
+  </p>
+  <p>
+    <input type="text" name="id_offer" value="" placeholder="productos en oferta">
+  </p>
+  <p>
+    <input type="image" name="imagen" value="">
+  </p>
+  <p>
+    <input type="text" name="remember_token" value="" placeholder="">
+  </p>  <p>
+      <input type="text" name="created_at" value="" placeholder="agregar producto">
+    </p>
+    <p>
+      <input type="text" name="updated_at" value="" placeholder="modificar producto">
+    </p>
+    <p>
+      <input type="text" name="deleted_at" value="" placeholder="eliminar producto">
+    </p>
+  <button type="submit">Enviar</button>
+  {{-- <button type="reset">Limpiar</button> --}}
+</form>
+
+
 
 </form>
 @endsection
