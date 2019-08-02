85458d6cbad5883cfb0d0385114922e70eddfae4
flor1985 <florgiles@hotmail.com>
2019-08-02 13:59:51 -0300

    se agregan imagenes al carusel, se arregla vista de productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,81 +12,85 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($category = null)
-    {
-      if($category){
-        $products = Product::all()->where('category', $category);
-        return view('products', compact('products'));
-      } else {
-      $products = Product::all(); //Traemos todos los productos.
-      return view('products', compact('products')); //Pasamos el array a la vista.
-      }
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-      $product = Product::find($id); //Identificamos el producto que queremos mostrar.
-      return view('product', compact('product')); //Pasamos el dato a la vista.
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(product $product)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, product $product)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(product $product)
-    {
-        //
-    }
+     public function index() 
+     {
+       return view('productos');
+     }
+    // public function index($category = null)
+    // {
+    //   if($category){
+    //     $products = Product::all()->where('category', $category);
+    //     return view('products', compact('products'));
+    //   } else {
+    //   $products = Product::all(); //Traemos todos los productos.
+    //   return view('products', compact('products')); //Pasamos el array a la vista.
+    //   }
+    // }
+    //
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
+    //
+    // /**
+    //  * Store a newly created resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
+    //
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  \App\product  $product
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show($id)
+    // {
+    //   $product = Product::find($id); //Identificamos el producto que queremos mostrar.
+    //   return view('product', compact('product')); //Pasamos el dato a la vista.
+    // }
+    //
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  \App\product  $product
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit(product $product)
+    // {
+    //     //
+    // }
+    //
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  \App\product  $product
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(Request $request, product $product)
+    // {
+    //     //
+    // }
+    //
+    // /**
+    //  * Remove the specified resource from storage.
+    //  *
+    //  * @param  \App\product  $product
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function destroy(product $product)
+    // {
+    //     //
+    // }
 }
