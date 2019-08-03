32ade589ee56cb56097133a7a3ee97cfb07633db
fiorefri <fiorefri@gmail.com>
2019-08-03 17:29:48 -0300

    Se crea las migraciones de carts y se elimina cartsProducts

--- app/Cart.php
+++ app/Cart.php
@@ -8,13 +8,18 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Cart extends Model
 {
   protected $guarded = [];
-  protected $fillable = ['product_id','name','price','cant_producto','user_id',];
+  // protected $fillable = ['product_id','name','price','cant_producto','user_id',];
 
     use softDeletes;
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
     }
 
     public function address()
--- database/migrations/2019_07_26_211454_create_carts_table.php
+++ database/migrations/2019_07_26_211454_create_carts_table.php
@@ -18,11 +18,16 @@ class CreateCartsTable extends Migration
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
+            $table->bigInteger('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('product');
+            $table->integer('cantidad_total')->unsigned();
+            $table->decimal('precio_total', 6,2)->unsigned();
+            $table->integer('estado')->default(0);
             $table->bigInteger('address_id')->unsigned();
             $table->foreign('address_id')->references('id')->on('addresses');
             $table->bigInteger('payment_id')->unsigned();
             $table->foreign('payment_id')->references('id')->on('payments');
-            $table->string('close_at');
+            $table->string('num_carrito')->nullable();
             $table->softDeletes();
         });
     }
