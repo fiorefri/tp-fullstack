3a60fd6b0ebfd084d2f379cbbf63e8cbf770694c
flor1985 <florgiles@hotmail.com>
2019-08-03 16:31:12 -0300

    se hacen modificaciones en quienes somos, faqs y contacto

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -36,9 +36,9 @@
           <nav class="main-navigation">
              <ul class="navigation">
                <!-- <li><a href="index.php">Home</a></li> -->
-               <li><a href="/quienes_somos">Quienes somos</a></li>
                <li><a href="/productos">Productos</a></li>
                <li><a href="/faq">Preguntas frecuentes</a></li>
+               <li><a href="/quienes_somos">Quienes somos</a></li>
                <li><a href="/contacto">Contacto</a></li>
              </ul>
            </nav>
@@ -55,9 +55,9 @@
         <h3>Menu</h3>
         <ul>
           <!-- <li><a href="index.php">Home</a></li> -->
-          <li><a href="/quienes_somos">Quienes somos</a></li>
           <li><a href="/productos">Productos</a></li>
           <li><a href="/faq">Preguntas frecuentes</a></li>
+          <li><a href="/quienes_somos">Quienes somos</a></li>
           <li><a href="/contacto">Contacto</a></li>
         </ul>
       </section>
@@ -83,7 +83,9 @@
       </section>
     </footer>
 
+
     <!-- Js -->
+    <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
--- resources/views/quienesSomos.blade.php
+++ resources/views/quienesSomos.blade.php
@@ -6,6 +6,7 @@
   <div class="about-us row d-flex justify-content-center">
   <div class="card  col-md-10">
   <div class="card-body">
+    <h3>Quienes somos</h3>
     <p class="card-text"><strong>Nature</strong> es una marca joven, enfocada en crear verdadera cosmética natural, cuidando hasta los mas mínimos detalles en cuanto a ingredientes, producción y envasado, utilizando materias primas naturales de la mas alta calidad. <br>A pesar del corto camino transitado, nuestro equipo tiene basta experiencia en el rubro de materias primas cosméticas.</p>
     <h5 class="card-title ">Por que decimos que creamos verdadera cosmética natural?</h5>
     <p class="card-text">En <strong>Nature</strong> elegimos rigurosamente cada materia prima utilizada en todas nuestras líneas, importamos nosotros mismos casi la totalidad de ingredientes, analizando previamente cada uno de ellos para certificar la calidad de los mismos.<br>Nuestros productos se fabrican a base de aceites vegetales puros, tales como aceite de almendras, aceite de coco, aceite de argán, aceite de jojoba, manteca de karité, entre otros. Evitamos en todo proceso y en nuestras formulas materia primas derivadas del petróleo, tales como parafina, vaselina, siliconas, entre otras.</p>
