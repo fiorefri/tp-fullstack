43aee87e3f896ca0be717691e500c82b7d4c768a
JudithGrau <judith.betina.grau@gmail.com>
2019-08-04 16:23:30 -0300

    Modificaciones en direcciones.blade

--- resources/views/direcciones.blade.php
+++ resources/views/direcciones.blade.php
@@ -3,17 +3,41 @@
 @section('titulo', 'Mis Direcciones')
 
 @section('principal')
-  @forelse ($address as $direccion)
-    <p>Calle:         {{$direccion->calle}}</p>
-    <p>Altura:        {{$direccion->altura}}</p>
-    <p>Piso:          {{$direccion->piso}}</p>
-    <p>Departamento:  {{$direccion->depto}}</p>
-    <p>Codigo Postal: {{$direccion->codigo_postal}}</p>
-    <p>Ciudad:        {{$direccion->ciudad}}</p>
-    <a href="/direcciones/editar/{{$direccion->id}}">Editar Dirección</a>
-  @empty
-    <p>No hay direcciones para mostrar.</p>
-  @endforelse
-  <a href="/direcciones/agregar">Agregar direcciones</a>
-  <a href="/cuenta">Ir a Mi Cuenta</a>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="direcciones card">
+        <div class="titulo-direcciones card-header">Mis direcciones</div>
+        <div class="card-body">
+          <div class="datos">
+            @forelse ($address as $direccion)
+            <p>Calle:   {{$direccion->calle}}</p>
+            <p>Altura:  {{$direccion->altura}}</p>
+            <p>Piso:   {{$direccion->piso}}</p>
+            <p>Departamento:  {{$direccion->depto}}</p>
+            <p>Código Postal:  {{$direccion->codigo_postal}}</p>
+            <p>Ciudad: {{$direccion->ciudad}}</p>
+            <div class="col-md-8 offset-md-4">
+              <button type="button" name="button" ><a href="/direcciones/editar/{{$direccion->id}}">
+                Editar Dirección</a></button>
+            </div>
+            <div class="col-md-8 offset-md-3">
+
+            @empty
+              <p>No hay direcciones para mostrar.</p>
+            @endforelse
+              <br>
+              <a href="/direcciones/agregar">Agregar direcciones / </a>
+              <a href="/cuenta">Ir a Mi Cuenta</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
 @endsection
