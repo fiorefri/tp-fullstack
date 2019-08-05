a91bb876cd8b5b482fdbf8f8445e9bc023f84e4d
fiorefri <fiorefri@gmail.com>
2019-08-05 03:58:49 -0300

    Se crea un historial de compras

--- resources/views/carrito/historial.blade.php
+++ resources/views/carrito/historial.blade.php
@@ -5,6 +5,13 @@
 @section('principal')
   <div class="">
     <p>Datos de compra:</p>
-    <p></p>
+    @forelse ($carts as $cart)
+      <img src="/storage/productos/{{$cart->imagen}}" alt="">
+      <p>Nombre:{{$cart->nombre}}</p>
+      <p>Cantidad: {{$cart->cantidad}}</p>
+      <p>Precio total: {{$cart->total}}</p>
+    @empty
+      <p>No hay productos para mostrar.</p>
+    @endforelse
   </div>
 @endsection
