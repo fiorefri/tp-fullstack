b6545b5f3ab865200be7adfb36c8b6894dd2147e
JudithGrau <judith.betina.grau@gmail.com>
2019-08-05 08:21:47 -0300

    modificacion en el historial.blade

--- resources/views/carrito/historial.blade.php
+++ resources/views/carrito/historial.blade.php
@@ -3,8 +3,17 @@
 @section('titulo', 'Historial de compras')
 
 @section('principal')
-  <div class="">
-    <p>Datos de compra:</p>
-    <p></p>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="historial card">
+          <div class="titulo-historial card-header">Datos de compra</div>
+
+        </div>
+
+      </div>
+
+    </div>
+
   </div>
 @endsection
