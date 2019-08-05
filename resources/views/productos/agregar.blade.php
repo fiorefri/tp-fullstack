422727ca6e92dd63004b9299bc0305bc6d845cd9
JudithGrau <judith.betina.grau@gmail.com>
2019-08-05 00:21:47 -0300

    modificaciones en productos agregar.blade

--- resources/views/productos/agregar.blade.php
+++ resources/views/productos/agregar.blade.php
@@ -5,42 +5,61 @@
 @section('class-body', 'container')
 
 @section('principal')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="formulario_agregar_productos card">
+        <div class="titulo-formulario card-header">Agregar Producto</div>
+        <div class="card-body">
+          <form class="" action="/productos/agregar" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="datos">
+              <p><input type="text" name="nombre" value="" placeholder="nombre producto"></p>
 
-<form class="" action="/productos/agregar" method="post" enctype="multipart/form-data">
-<h2>Agregar producto producto</h2>
-<form class="" action="/productosAgregar" method="post" enctype="multipart/form-data">
-  @csrf
-  <p>
-    <input type="text" name="nombre" value="" placeholder="nombre producto">
-  </p>
-  <p>
-    <input type="text" name="descripcion" value="" placeholder="descripción producto">
-  </p>
-  <p>
-    <input type="number_format" name="precio" value="" placeholder="precio producto">
-  </p>
-  <p>
-    <input type="number" name="stock" value="" placeholder="stock">
-  </p>
-  <p>
-    <select class="" name="category_id">
-      @foreach($categorias as $categoria)
-      <option value="{{$categoria->id}}">{{$categoria->nombre}}</option>
-      @endforeach
+              <p><input type="text" name="descripcion" value="" placeholder="descripción producto"></p>
 
-    </select>
-  </p>
-  <p>
-    <input type="text" name="offer_id" value="" placeholder="productos en oferta">
-  </p>
-  <p>
-    <input type="file" name="imagen" value="">
-  </p>
+              <p>
+                <input type="number_format" name="precio" value="" placeholder="precio producto">
+              </p>
 
-  <button type="submit">Enviar</button>
-  {{-- <button type="reset">Limpiar</button> --}}
-</form>
+              <p>
+                <input type="number" name="stock" value="" placeholder="stock">
+              </p>
 
+              <p>
+                <select class="" name="category_id">
+                  @foreach($categorias as $categoria)
+                  <option value="{{$categoria->id}}">{{$categoria->nombre}}</option>
+                  @endforeach
 
-</form>
+                </select>
+              </p>
+
+              <p>
+                <input type="text" name="offer_id" value="" placeholder="productos en oferta">
+              </p>
+
+              <p>
+                <input type="file" name="imagen" value="">
+              </p>
+
+            </div>
+            <div class="form-group row mb-0">
+              <div class="col-md-8 offset-md-4">
+                <button type="submit">Enviar</button>
+                {{-- <button type="reset">Limpiar</button> --}}
+
+              </div>
+
+            </div>
+          </form>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
 @endsection
